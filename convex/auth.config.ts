bf81f9f250e88a4c50e1b4c506d4728007bd5d47
Jeff <lmh.jeff@gmail.com>
2025-05-25 02:09:43 +0100

    first commit, set up convex, shadcn

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  providers: [
+    {
+      domain: 'https://simple-foxhound-99.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
---
+++ types.ts
@@ -0,0 +1,9 @@
+export type Event = {
+  name: string;
+  description: string;
+  price: number;
+  totalTickets: number;
+  location: string;
+  eventDate: number;
+  userId: string;
+};
