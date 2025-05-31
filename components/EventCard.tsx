48536deea658785564cb2761cb3cd3b0a47ebe10
Jeff <lmh.jeff@gmail.com>
2025-05-31 17:39:27 +0100

    added detail

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -6,17 +6,21 @@ import { useUser } from '@clerk/clerk-react';
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
 import { useStorageUrl } from '@/lib/utils';
+import Image from 'next/image';
+import { CalendarDays, MapPin, StarIcon, Ticket } from 'lucide-react';
 
 const EventCard = ({ eventId }: { eventId: Id<'events'> }) => {
   const { user } = useUser();
   const router = useRouter();
 
-  const events = useQuery(api.events.getEventById, { eventId });
+  const event = useQuery(api.events.getEventById, { eventId });
   const availability = useQuery(api.events.getEventAvailability, { eventId });
   const userTicket = useQuery(api.tickets.getUserTicketForEvent, {
     eventId,
     userId: user?.id ?? '',
   });
+
+  const isEventOwner = user?.id === event?.userId;
 
   const queuePosition = useQuery(api.waitingList.getQueuePosition, {
     eventId,
@@ -39,7 +43,91 @@ const EventCard = ({ eventId }: { eventId: Id<'events'> }) => {
         isPastEvent ? 'opacity-75 hover:opacity-100' : ''
       }`}
     >
-      events
+      {/* Event Image */}
+      {imageUrl && (
+        <div className='relative w-full h-48'>
+          <Image
+            src={imageUrl}
+            alt={event.name}
+            fill
+            className='object-cover'
+            priority
+          />
+          <div className='absolute inset-0 bg-gradient-to-t from-black/50 to-transparent' />
+        </div>
+      )}
+
+      {/* event details */}
+      <div className={`p-6 bg-red-300 ${imageUrl ? 'relative' : ''}`}>
+        <div className='flex items-start justify-between'>
+          {/* event name and owener badge */}
+          <div>
+            <div className='flex flex-col items-start gap-2'>
+              {isEventOwner && (
+                <span className='inline-flex items-center gap-1 bg-blue-500/90 text-white px-2 py-1 rounded-full text-xs font-medium'>
+                  <StarIcon className='w-3 h-3' />
+                  Your Event
+                </span>
+              )}
+              <h2 className='text-2xl font-bold text-gray-900'>{event.name}</h2>
+            </div>
+            {isPastEvent && (
+              <span className='inline-flex items-center px-2.5 py-0.5  bg-gray-100/90 text-gray-800 mt-2 rounded-full text-xs font-medium'>
+                Past event
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* Price Tag */}
+      <div className='flex flex-col items-end gap-2 ml-4'>
+        <span
+          className={`px-4 py-1.5 font-semibold rounded-full ${
+            isPastEvent
+              ? 'bg-gray-50 text-gray-500'
+              : 'bg-green-50 text-green-700'
+          }`}
+        >
+          £{event.price.toFixed(2)}
+        </span>
+        {availability.purchasedCount >= availability.totalTickets && (
+          <span className='px-4 py-1.5 bg-red-50 text-red-700 font-semibold rounded-full text-sm'>
+            Sold Out
+          </span>
+        )}
+      </div>
+
+      {/* event detail */}
+      <div className='mt-4 gap-3 bg-amber-300'>
+        <div className='flex items-center text-gray-600'>
+          <MapPin className='w-4 h-4 mr-2' />
+          <span>{event.location}</span>
+        </div>
+      </div>
+
+      <div className='flex items-center text-gray-600'>
+        <CalendarDays className='w-4 h-4 mr-2' />
+        <span>
+          {new Date(event.eventDate).toLocaleDateString()}{' '}
+          {isPastEvent && '(Ended)'}
+        </span>
+      </div>
+
+      <div className='flex items-center text-gray-600'>
+        <Ticket className='w-4 h-4 mr-2' />
+        <span>
+          {availability.totalTickets - availability.purchasedCount} /{' '}
+          {availability.totalTickets} available
+          {!isPastEvent && availability.activeOffers > 0 && (
+            <span className='text-amber-600 text-sm ml-2'>
+              ({availability.activeOffers}{' '}
+              {availability.activeOffers === 1 ? 'person' : 'people'} trying to
+              buy)
+            </span>
+          )}
+        </span>
+      </div>
     </div>
   );
 };
